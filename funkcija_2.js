8b8c485d03bcd3f8d50b81c9c1241179b4375945
ArvydasKacinas <arvydaskacinas@gmail.com>
2022-07-07 21:34:25 +0300

    funkcija2

---
+++ funkcija_2.js
@@ -0,0 +1,12 @@
+'use-strict';
+let a;
+let b;
+function saknis(a,b){
+    for(i=a; i<b; i++){
+        let skaiciausSaknis;
+        skaiciausSaknis=Math.sqrt(i);
+        console.log(skaiciausSaknis);
+    }
+};
+
+saknis(0,170);
